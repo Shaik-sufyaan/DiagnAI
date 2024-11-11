d49bef5e31b5566bf5a7b40e7827cf5d111eb50b
Varun Ahlawat <110617721+yup-VARUN@users.noreply.github.com>
2024-11-10 20:12:29 -0500

    unifying the VectorDB and VoyageEmbedding classes into one single class

---
+++ test_new.py
@@ -0,0 +1,30 @@
+import rag as r
+import dotenv
+import os
+
+dotenv.load_dotenv()
+voyage_api = os.getenv("VOYAGE_API_KEY")
+
+data = r.Data_Handler(voyage_api)
+
+text_directory = "/Users/varunahlawat/AI_ATL_LOCAL/DataCorpus"
+
+############ VECTOR DB ############
+vector_directory = "/Users/varunahlawat/AI_ATL_LOCAL/VectorDB"
+
+if not os.path.exists(vector_directory):
+    size = 0
+    print("VectorDB directory doesn't exist; creating it now...")
+    vector_db = data.create_vector_db(vector_directory)
+else:
+    size = data.collection_size
+
+print(size)
+print(type(size))
+
+# vector_chunks = data.document_load(text_directory)
+
+# for i in range(size , size + len(vector_chunks)+1):
+#     doc_id = f"doc_{i}"
+#     data.append(vector_chunks, doc_id)
+
